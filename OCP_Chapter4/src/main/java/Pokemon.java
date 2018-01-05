e340be3c27e4ecafb10718d9a3d8198ea1296328
ssoegriemsingh <ssoegriemsingh@qualogy.com>
2018-01-05 15:13:08 +0100

    final commit

--- OCP_Chapter4/src/main/java/Pokemon.java
+++ OCP_Chapter4/src/main/java/Pokemon.java
@@ -3,24 +3,52 @@ public class Pokemon {
     private final String type;
     private final int currentEvolutionStage;
     private final int maximumEvolutionStage;
-    private final boolean gender;
-    private final String abilitie;
+    private final String gender;
+    private final String ability;
     private final int numberOfAttacks;
     private final String trainer;
 
     public Pokemon(String name, String type, int currentEvolutionStage, int maximumEvolutionStage, String gender,
-                   String abilitie, int numberOfAttacks, String trainer) {
+                   String ability, int numberOfAttacks, String trainer) {
         this.name = name;
         this.type = type;
         this.currentEvolutionStage = currentEvolutionStage;
         this.maximumEvolutionStage = maximumEvolutionStage;
-        this.gender = checkGender(gender);
-        this.abilitie = abilitie;
+        this.gender = gender;
+        this.ability = ability;
         this.numberOfAttacks = numberOfAttacks;
         this.trainer = trainer;
     }
 
-    private boolean checkGender(String gender) {
-        return false;
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getCurrentEvolutionStage() {
+        return currentEvolutionStage;
+    }
+
+    public int getMaximumEvolutionStage() {
+        return maximumEvolutionStage;
+    }
+
+    public String getAbility() {
+        return ability;
+    }
+
+    public int getNumberOfAttacks() {
+        return numberOfAttacks;
+    }
+
+    public String getTrainer() {
+        return trainer;
+    }
+
+    public String getGender() {
+        return gender;
     }
 }
